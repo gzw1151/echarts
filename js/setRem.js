172a7d05b6fac36882829e5fd58b761b27f2ddfa
郭宗卫 <317657406@qq.com>
2022-11-25 21:43:04 +0800

    '完成'

---
+++ js/setRem.js
@@ -0,0 +1,12 @@
+function setRem() {
+    var clientWidht = document.clientWidht || document.body.clientWidth;
+    clientWidht = clientWidht > 1920 ? 1920 : clientWidht;
+    clientWidht = clientWidht < 1024 ? 1024 : clientWidht;
+    var rem = clientWidht / 19.2;
+    var html = document.getElementsByTagName('html')[0];
+    html.style.fontSize = rem + 'px';
+
+}
+
+window.onload = setRem;
+window.onresize = setRem;
